00cc4d21d62d390f1dbfb63c5c77f6fdd9b68a7a
geregr <rockyliu20110221@gmail.com>
2020-11-08 11:57:25 -0800

    first commit ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./conponents/Header";
+import Container from "@material-ui/core/Container";
+import Button from "react-bootstrap/Button";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import { useState } from "react";
+import { render } from "react-dom";
 
 function App() {
+  const [blue, setblue] = useState(100);
+  const [red, setred] = useState(100);
+  const [tiyufkvgjmhbn, tiyufkvgjmhbne] = useState("AMONGUSBATTLEOFIMPOSTAS");
+  const attackBlue = () => {
+    if (blue < 0) {
+      alert("red win !");
+      
+    } else {
+      var num1=Math.floor(Math.random() * 15);
+      tiyufkvgjmhbne("Attack blue by " +  num1);
+      setblue(blue - num1);
+    }
+  };
+  const attackRed = () => {
+    if (red < 0) {
+      alert("blue win !");
+    } else {
+      var num = Math.floor(Math.random() * 15);
+      setred(red - num);
+      tiyufkvgjmhbne("Attack red by " + num);
+    }
+  };
+  const healBlue = () => {
+    if (blue > 100) {
+      alert("you can't heal no more");
+    } else {
+      setblue(blue + Math.floor(Math.random() * 15));
+    }
+  };
+  const healRed = () => {
+    if (red > 100) {
+      alert("you can't heal no more");
+    } else {
+      setred(red + Math.floor(Math.random() * 15));
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Container maxWidth="lg">
+        <h1 className="title" id="tiyufkvgjmhbn">
+          {tiyufkvgjmhbn}
+        </h1>
+      </Container>
+      <div className="bob-the-builder">
+        <div className="bob-the-player">
+          <h1>Blue Imposter</h1>
+          <Button variant="outline-success" onClick={healBlue}>
+            Heal
+          </Button>{" "}
+          <Button variant="outline-danger" onClick={attackRed}>
+            Attack
+          </Button>{" "}
+          <ProgressBar striped variant="success" key={1} animated now={blue} />
+          <img
+            src="https://media2.giphy.com/media/I2flwH2OIk1iJmeQo4/giphy.webp?cid=ecf05e472wzxap3iyk50jgkgr2sxvopuf6ytsx1jqxl7paub&rid=giphy.webp"
+            alt=""
+          />
+        </div>
+
+        <div className="bob-the-monster">
+          <h1>Red Imposter</h1>
+          <Button variant="outline-success" onClick={healRed}>
+            Heal
+          </Button>{" "}
+          <Button variant="outline-danger" onClick={attackBlue}>
+            Attack
+          </Button>{" "}
+          <ProgressBar striped variant="success" key={1} animated now={red} />
+          <img
+            className="imposta"
+            src="https://media2.giphy.com/media/ipDBtBngV1dubObG9N/giphy.webp?cid=ecf05e47v7wcx8uzcaavva1pyomy631ek858le0k3fu64dtz&rid=giphy.webp"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/conponents/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+
+export default function Header(){
+    return (
+        <div>
+            <h1 className="yo">imposter attacker</h1>
+            
+        </div>
+    )
+}
